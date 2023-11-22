bd10d697fea0fad4c99ab2f36acd197f1d87c9a9
Mateo Rosello <104503379+elr0s@users.noreply.github.com>
2023-11-21 21:15:13 -0300

    fix manejador de filtors

--- CodigoTPO/src/models/habitacion/ManejadorDeFiltros.java
+++ CodigoTPO/src/models/habitacion/ManejadorDeFiltros.java
@@ -1,22 +1,18 @@
 package models.habitacion;
 
-import controllers.ControllerHabitacion;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class ManejadorDeFiltros {
     private FiltrosHabitacion primerFiltro;
-    private ControllerHabitacion controllerHabitacion = ControllerHabitacion.getInstancia();
-    public ManejadorDeFiltros(FiltrosHabitacion primerFiltro) {
+    public ManejadorDeFiltros() {
         FiltrosHabitacion filtroExtras    = new FiltrarExtras(null);
         FiltrosHabitacion filtroHuespuedes = new FiltroCantidadHuespedes(filtroExtras);
         FiltrosHabitacion filtroTipoHab    = new FiltroTipoHabitacion(filtroHuespuedes);
         this.primerFiltro = filtroHuespuedes;
     }
 
-    public List<Habitacion> filtrarHabitacion(int cantidad, TipoHabitacion tipo,List<Extra> extra){
-        return primerFiltro.filtrarHabitacion(cantidad,tipo,extra,controllerHabitacion.getListadoHabitaciones());
+    public List<Habitacion> filtrarHabitacion(int cantidad, TipoHabitacion tipo,List<Extra> extra,List<Habitacion> habitaciones){
+        return primerFiltro.filtrarHabitacion(cantidad,tipo,extra,habitaciones);
     };
 
 }
